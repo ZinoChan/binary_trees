f6ac8b1169c2a8adc929ad7be103d9449640c53b
ZinoChan <devzinochan@gmail.com>
2023-08-01 13:04:12 +0100

    fix betty

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,13 +7,13 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    int l_height, r_height;
+	int l_height, r_height;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    l_height = (tree->left) ? (int)binary_tree_height(tree->left) : -1;
-    r_height = (tree->right) ? (int)binary_tree_height(tree->right) : -1;
+	l_height = (tree->left) ? (int)binary_tree_height(tree->left) : -1;
+	r_height = (tree->right) ? (int)binary_tree_height(tree->right) : -1;
 
-    return (l_height - r_height);
+	return (l_height - r_height);
 }
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,10 +8,11 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t l_height, r_height;
-    if (tree == NULL)
-        return (0);
-    l_height = binary_tree_height(tree->left);
-    r_height = binary_tree_height(tree->right);
 
-    return (l_height > r_height ? l_height + 1 : r_height + 1);
+	if (tree == NULL)
+		return (0);
+	l_height = binary_tree_height(tree->left);
+	r_height = binary_tree_height(tree->right);
+
+	return (l_height > r_height ? l_height + 1 : r_height + 1);
 }
